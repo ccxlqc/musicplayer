e0af1d93275cf61f07ee23c7c8602d27c3ac5de5
ccxlqc <1337994319@qq.com>
2018-07-07 13:33:01 +0800

    导航栏

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import fastclick from 'fastclick'
 import router from './router'
+import 'common/stylus/index.styl'
 
-Vue.config.productionTip = false
-
+fastclick.attach(document.body)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
